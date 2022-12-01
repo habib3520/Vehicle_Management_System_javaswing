fea6492bd49b94907c722848d2a49b5b78462518
Md Habibullah <77206446+habib3520@users.noreply.github.com>
2022-12-02 02:21:37 +0600

    Add files via upload

---
+++ src/vms/Vms_main.java
@@ -0,0 +1,15 @@
+
+package vms;
+
+public class Vms_main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Main_login_design l= new Main_login_design();
+        l.setVisible(true);
+        System.out.println("habib");
+        
+    }
+    
+}
